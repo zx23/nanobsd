0721b4f7dca66aa7e3500922d196c087202387b3
Andrew Wasilczuk <akw@scalefactory.com>
2017-01-07 16:35:28 +0000

    More general basebox name

--- build.sh
+++ build.sh
@@ -40,17 +40,17 @@ if [ "${VAGRANT_IMAGE}" = true ] ; then
     rm -f nanobsd.vdi && VBoxManage convertdd nanobsd.full nanobsd.vdi --format VDI
 
     echo '### Creating VirtualBox VM'
-    VBoxManage createvm --name alixbasebox --register
-    VBoxManage modifyvm alixbasebox --ostype FreeBSD --memory 256 \
+    VBoxManage createvm --name nanobasebox --register
+    VBoxManage modifyvm nanobasebox --ostype FreeBSD --memory 256 \
         --nic1 nat --nictype1 82540EM --nic2 intnet --nictype2 82540EM --nic3 intnet --nictype3 82540EM \
         --cableconnected1 on --cableconnected2 on --cableconnected3 on \
         --uart1 0x3F8 4 --uartmode1 disconnected
-    VBoxManage storagectl alixbasebox --name IDE --add ide --controller PIIX4
-    VBoxManage storageattach alixbasebox --storagectl IDE --port 0 --device 0 --type hdd --medium nanobsd.vdi
+    VBoxManage storagectl nanobasebox --name IDE --add ide --controller PIIX4
+    VBoxManage storageattach nanobasebox --storagectl IDE --port 0 --device 0 --type hdd --medium nanobsd.vdi
 
     echo '### Packaging vagrant base box'
-    rm -f nanobsd.box && vagrant package --base alixbasebox --output nanobsd.box
+    rm -f nanobsd.box && vagrant package --base nanobasebox --output nanobsd.box
 
     echo '## Cleaning up'
-    VBoxManage unregistervm alixbasebox --delete
+    VBoxManage unregistervm nanobasebox --delete
 fi
